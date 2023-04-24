e731fd5738e185c4f7de56c19bbc7d4de6341f4c
bs170400637 <111299722+bs170400637@users.noreply.github.com>
2023-04-24 15:52:29 +0400

    Add files via upload

---
+++ restaurant/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Menu, Booking
+from django.contrib.auth.models import User
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['Title', 'Price', 'Inventory']
+        template_name = 'index.html'
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'groups']
